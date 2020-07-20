60a9744c05a347fe580fe26e598b1d3bb258736f
weilin.cao <weilin.cao@langlive.com>
2020-07-20 18:52:32 +0800

    gin -blog

---
+++ pkg/util/pagination.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+
+	"gin-blog/pkg/setting"
+)
+/**
+ 获取offset
+ 第一页 offset 0 10
+ 第二页 offset 10 10
+ 第三页 offset 20 10
+ */
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.PageSize
+	}
+
+	return result
+}
